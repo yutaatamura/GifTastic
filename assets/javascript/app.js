b53d605f66824b99a5224dc9c8d63fffa68a93f5
Yuta Tamura <ytamura@shin-tech.com>
2018-03-05 17:16:02 -0600

    Add variables to prepare for gif stop/start functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,57 @@
 $(document).ready(function() {
 
-topics = [
-    "raccoons",
-    "cats",
-    "corgi"
-];
+    topics = [];
 
-function createButtons() {
-    $('#buttons-container').empty();
+    function createButtons() {
+        $('#buttons-container').empty();
 
-    for (var i=0; i< topics.length; i++) {
-        var button = $("<button>");
-        button.addClass("topic-btn");
-        button.attr("data-name", topics[i]);
-        button.text(topics[i]);
-        $('#buttons-container').append(button);
+        for (var i=0; i< topics.length; i++) {
+            var button = $("<button>");
+            button.addClass("topic-btn");
+            button.attr("data-name", topics[i]);
+            button.text(topics[i]);
+            $('#buttons-container').append(button);
+        }
     }
-}
 
 
-function displayGif() {
+    function displayGif() {
 
 
-var topic = $(this).attr("data-name");
-console.log(topic);
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=641SVLMQJGCVM4YinHwMvsgdZ3Sd1RJi&limit=10";
-console.log(queryURL); 
+        var topic = $(this).attr("data-name");
+        console.log(topic);
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=641SVLMQJGCVM4YinHwMvsgdZ3Sd1RJi&limit=10";
+        console.log(queryURL); 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-console.log(response)
-var gifResults = response.data;
-for (var i = 0; i<gifResults.length;i++) {
-    var source = gifResults[i].images.fixed_height_downsampled.url;
-    var showGif = $("<img>");
-    showGif.attr("src", source);
-    $('#gif-container').append(showGif);
-}
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+        console.log(response)
+        var gifResults = response.data;
+        for (var i = 0; i<gifResults.length;i++) {
+            var stillGif = gifResults[i].images.fixed_height_small_still.url;
+            var dyanmicGif = gifResults[i].images.fixed_height_small.url;
+            var rating = gifResults[i].rating;
+            var showGif = $("<img>");   
+            showGif.attr("src", stillGif);
+            showGif.attr("data-gif", dynamicGif)
+            $('#gif-container').append(showGif);
+            $('#gif-container').append("<p> Rating: " + rating + "</p>");
+        }
+         
+        })
 
-    
-})
+    }
 
-}
+    $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+        var topic = $('#topic-input').val().trim();
+        topics.push(topic);
+        createButtons();
+    })
 
-$("#add-topic").on("click", function(event) {
-    event.preventDefault();
-    var topic = $('#topic-input').val().trim();
-    topics.push(topic);
-    createButtons();
-})
-
-$(document).on("click", ".topic-btn", displayGif);
+    $(document).on("click", ".topic-btn", displayGif);  
 
 
 

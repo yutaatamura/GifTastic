b2df1c1431e691276b6a4594d9e07d44f4c1f8fe
Yuta Tamura <ytamura@shin-tech.com>
2018-03-06 16:50:10 -0600

    Working on appending an element within an appended element...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
 
     function displayGif() {
 
-
         var topic = $(this).attr("data-name");
         console.log(topic);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=641SVLMQJGCVM4YinHwMvsgdZ3Sd1RJi&limit=10";
@@ -29,19 +28,54 @@ $(document).ready(function() {
         }).then(function(response) {
         console.log(response)
         var gifResults = response.data;
-        for (var i = 0; i<gifResults.length;i++) {
-            var stillGif = gifResults[i].images.fixed_height_small_still.url;
-            var dyanmicGif = gifResults[i].images.fixed_height_small.url;
-            var rating = gifResults[i].rating;
-            var showGif = $("<img>");   
-            showGif.attr("src", stillGif);
-            showGif.attr("data-gif", dynamicGif)
-            $('#gif-container').append(showGif);
-            $('#gif-container').append("<p> Rating: " + rating + "</p>");
-        }
+        
+        var rowIndex = 0;
+        var colIndex = 0;
+            for (var i = 0; i<gifResults.length;i++) {
+                var stillGif = gifResults[i].images.fixed_height_small_still.url;
+                var dynamicGif = gifResults[i].images.fixed_height_small.url;
+                var rating = gifResults[i].rating;
+    
+                
+                
+    
+                var showGif = $("<img>");   
+                showGif.attr("src", stillGif);
+                showGif.attr("data-still", stillGif);
+                showGif.attr("class", "topicGif img-thumbnail");
+                showGif.attr("data-dynamic", dynamicGif);
+                showGif.attr("data-index", i);
+            
+                if (colIndex === 4 || colIndex === 0) {
+                    colIndex === 0;
+                    console.log('i am colIndex='+colIndex)
+                    $('#gif-container').append(gifRowWrap);
+                    
+                    var gifRowWrap = $("<div>");
+                    gifRowWrap.attr("class", "row");
+                    gifRowWrap.attr("id", "gifRow"+rowIndex);
+                    console.log('i am rowIndex='+rowIndex)
+                    $('#gifRow'+rowIndex).append(gifDivWrap);
+                    console.log('i appended the div')
+                    var gifDivWrap = $("<div>");
+                    gifDivWrap.attr("class", "col-md-3");
+                    gifDivWrap.attr("id", "gifDiv"+i);
+                    colIndex++;
+                    console.log(colIndex)
+                    rowIndex++;
+                    $('#gifDiv'+i).append(showGif);
+                    $('#gifDiv'+i).append("<p> Rating: " + rating + "</p>");
+                } else {
+                    $('#gifRow'+rowIndex).append(gifDivWrap);
+                    colIndex++;
+                    console.log('colindex is='+colIndex)
+                    $('#gifDiv'+i).append(showGif);
+                    $('#gifDiv'+i).append("<p> Rating: " + rating + "</p>");
+                }
          
+            }   
         })
-
+    
     }
 
     $("#add-topic").on("click", function(event) {
@@ -53,8 +87,23 @@ $(document).ready(function() {
 
     $(document).on("click", ".topic-btn", displayGif);  
 
+//Make the GIF dynamic on click 
+    $(document).on("click", ".topicGif", function(event) {
+        var placeHolder = $(this).attr("data-index");
+        console.log(placeHolder);
+        var stillGifURL = $(this).attr("data-still");
+        console.log(stillGifURL);
+        var dynamicGifURL = $(this).attr("data-dynamic");
+        console.log(dynamicGifURL);
 
-
+        if ($(this).attr("src") === stillGifURL) {
+            $(this).attr("src", dynamicGifURL)
+        } else if ($(this).attr("src") === dynamicGifURL) {
+            $(this).attr("src", stillGifURL)
+        }
+            
+        
+    })
 
 
 

dd2a9723980ebdfe1d03cc6354d50bfab0336bce
Yuta Tamura <ytamura@shin-tech.com>
2018-03-08 10:00:50 -0600

    Reorganized row and col structure to separate between topics. Included function to delete the previous clicked topic. Included function to prevent user from creating two identical buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
     topics = [];
+    var rowIndex = 0;
+    var colIndex = 0;
+    var lastClick;
 
     function createButtons() {
         $('#buttons-container').empty();
@@ -17,10 +20,13 @@ $(document).ready(function() {
 
     function displayGif() {
 
-        var topic = $(this).attr("data-name");
+        var topic = $(this).attr("data-name");  
         console.log(topic);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=641SVLMQJGCVM4YinHwMvsgdZ3Sd1RJi&limit=10";
         console.log(queryURL); 
+        lastClick = $(this).attr("data-name");
+        console.log("lastclick="+lastClick)
+         
 
         $.ajax({
             url: queryURL,
@@ -29,8 +35,7 @@ $(document).ready(function() {
         console.log(response)
         var gifResults = response.data;
         
-        var rowIndex = 0;
-        var colIndex = 0;
+        
             for (var i = 0; i<gifResults.length;i++) {
                 var stillGif = gifResults[i].images.original_still.url;
                 // fixed_height_small_still.url;
@@ -47,7 +52,8 @@ $(document).ready(function() {
 
                 var gifDivWrap = $("<div>");
                 gifDivWrap.attr("class", "col-md-3 "+topic+"");
-                gifDivWrap.attr("id", "gifDiv"+i);
+                gifDivWrap.attr("id", topic+"gifDiv"+i);
+
                 if (colIndex === 4 || colIndex === 0) {
                     colIndex = 0;
                     console.log('i am colIndex='+colIndex)
@@ -69,18 +75,24 @@ $(document).ready(function() {
                     colIndex++;
                     console.log(colIndex)
                     
-                    $('#gifDiv'+i).append(showGif);
-                    $('#gifDiv'+i).append("<p> Rating: " + rating + "</p>");
+                    //append the gif and rating to the div;
+                    $('#'+topic+'gifDiv'+i).append(showGif);
+                    $('#'+topic+'gifDiv'+i).append("<p> Rating: " + rating + "</p>");
                 } else {
 
                     $('#gifRow'+rowIndex).append(gifDivWrap);
                     colIndex++;
                     console.log('colindex is='+colIndex)
-                    $('#gifDiv'+i).append(showGif);
-                    $('#gifDiv'+i).append("<p> Rating: " + rating + "</p>");
+                    $('#'+topic+'gifDiv'+i).append(showGif);
+                    $('#'+topic+'gifDiv'+i).append("<p> Rating: " + rating + "</p>");
+
+                    
                 }
-         
-            }   
+                
+            }  
+            colIndex = 0;
+            console.log("return colIndex to "+colIndex)
+             
         })
     
     }
@@ -88,7 +100,17 @@ $(document).ready(function() {
     $("#add-topic").on("click", function(event) {
         event.preventDefault();
         var topic = $('#topic-input').val().trim();
+        var search = topics.indexOf(topic);
+        if (search !== -1) {
+            $('#messageDiv').text("You have already created this topic! Try another.");
+            topics.pop();
+            return;
+        }
+        
         topics.push(topic);
+        console.log("topics"+topics)
+        
+        console.log("search="+search)
         if (topic === null || topic === "") {
             $('#messageDiv').text("Please input a topic!");
             console.log(topics)
@@ -104,12 +126,25 @@ $(document).ready(function() {
 
     $('#delete-topic').on("click", function(event) {
         event.preventDefault();
-        var topic = topics.pop();
+        console.log("topics array="+topics)
         createButtons();
+        console.log("i will delete lastclick="+lastClick)
         // $('.'+topic+'').remove();
-        $('.'+topic+'').remove();
+        $('.'+lastClick+'').remove();
+        
+        
+        arraySplice();
+        console.log("remaining in topics array="+topics)
     
     })
+
+    function arraySplice() {
+        console.log("i am in arraySplice and lastclick is="+lastClick)
+        var index = topics.indexOf(lastClick);
+        if (index !== -1) {
+            topics.splice(index, 1);
+        }
+    }
 
     $(document).on("click", ".topic-btn", displayGif);  
 
